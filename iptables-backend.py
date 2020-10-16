0558a231223c109cf3b6c88f836db9e7bc7b5bc2
Masoud Sadeghi <35397912+pyDeb@users.noreply.github.com>
2020-10-16 16:01:57 -0400

    Create iptables-backend.py

---
+++ iptables-backend.py
@@ -0,0 +1,1 @@
+### There was a Python back-end here, but due to my contract, I had to remove this file. However, I can prove it to you in an interview that I know about iptables and how to configure it :)
